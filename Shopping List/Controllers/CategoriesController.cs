4bd846d32d106d270dc4cd7c2399ca4d5c3208c3
Leabenisty <lea32533@gmail.com>
2025-06-23 19:50:55 +0300

    restart and build models

---
+++ Shopping List/Controllers/CategoriesController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Shopping_List.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+
+        // GET: api/<CategoriesController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+        //public async Task<ActionResult<IEnumerable<ItemDto>>> GetAllItems()
+        //{
+        //    var items = await _itemService.GetAllItemsAsync();
+        //    if (items == null || items.Count == 0)
+        //    {
+        //        return Ok(new
+        //        {
+        //            Status = "success",
+        //            Message = "לא נמצאו פריטים",
+        //            Data = new List<ItemDto>()
+        //        });
+        //    }
+        //    return Ok(new
+        //    {
+        //        Status = "success",
+        //        Message = "הפריטים נשלפו בהצלחה",
+        //        Data = items
+        //    });
+        //}
+
+    }
+}
